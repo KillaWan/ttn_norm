b1e8fb5a453ea0b4695181cfcfb37b0b83a2c4e0
KillaWan <Killa_wan@outlook.com>
2026-02-26 21:26:22 +0100

    Revert "v8"
    This reverts commit 8448259883b991bf76ef268b6359ce3b660f0472.

--- models/__init__.py
+++ models/__init__.py
@@ -1,11 +1,5 @@
 from .local_tf_norm.local_norm import LocalTFNorm, LocalTFNormState
-from .local_gain_norm import LocalGainNorm
 from .ttn_model import TTNModel
 from ttn_norm.normalizations import DishTS, FAN, No, RevIN, SAN
 
-__all__ = [
-    "LocalTFNorm", "LocalTFNormState",
-    "LocalGainNorm",
-    "TTNModel",
-    "RevIN", "FAN", "DishTS", "SAN", "No",
-]
+__all__ = ["LocalTFNorm", "LocalTFNormState", "TTNModel", "RevIN", "FAN", "DishTS", "SAN", "No"]
