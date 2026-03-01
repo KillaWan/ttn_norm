0e17936d61c4a2b2cbefeb96b35bb5ce1826aad2
KillaWan <Killa_wan@outlook.com>
2026-03-01 21:15:27 +0100

    fix bugs

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,5 @@
 from .local_tf_norm.local_norm import LocalTFNorm, LocalTFNormState
 from .ttn_model import TTNModel
-from ttn_norm.normalizations import DishTS, FAN, No, RevIN, SAN
+from ttn_norm.normalizations import DishTS, FAN, No, RevIN, SAN, SANMS
 
-__all__ = ["LocalTFNorm", "LocalTFNormState", "TTNModel", "RevIN", "FAN", "DishTS", "SAN", "No"]
+__all__ = ["LocalTFNorm", "LocalTFNormState", "TTNModel", "RevIN", "FAN", "DishTS", "SAN", "SANMS", "No"]
